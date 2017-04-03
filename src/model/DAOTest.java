9b8fdfbcfb52d0beb50b1abd506a620489686545
Vyacheslav Stroev <stroev.vyacheslav@gmail.com>
2017-04-03 20:16:43 +0300

    Added tests.

--- src/model/DAOTest.java
+++ src/model/DAOTest.java
@@ -3,6 +3,7 @@ package model;
 import org.hibernate.HibernateException;
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
+import org.testng.Assert;
 
 import java.sql.Date;
 import java.util.List;
@@ -24,14 +25,11 @@ public class DAOTest {
         RentServiceDAOImpl rs = new RentServiceDAOImpl();
         Integer size = null;
         List<Film> list = null;
-        try {
-            list = rs.getAllFilms();
-        }
-        catch (HibernateException e) {
-            assert(false);
-        }
+
+        list = rs.getAllFilms();
+
         size = list.size();
-        assert(size != null);
+        Assert.assertNotNull(size);
 
         Film f = new Film();
         f.setName(name);
@@ -39,30 +37,27 @@ public class DAOTest {
         f.setProducer(producer);
         f.setCdCost(cost);
         f.setCasetteCost(cost);
-        try {
-            rs.addFilm(f);
 
-        }
-        catch (HibernateException e) {
-            assert(false);
-        }
+        rs.addFilm(f);
 
-        try {
-            assert(rs.getAllFilms().size() == size + 1);
-            Film tmp = rs.loadFilm(f.getId());
-            assert(tmp != null);
-            assert(equals(tmp.getId(), f.getId()));
-            assert(equals(tmp.getName(), f.getName()));
-            assert(equals(tmp.getCompany(), f.getCompany()));
-            assert(equals(tmp.getProdusr(), f.getProdusr()));
-            assert(equals(tmp.getCdCost(), f.getCdCost()));
-            assert(equals(tmp.getCasetteCost(), f.getCasetteCost()));
-            rs.deleteFilm(tmp);
-            assert(rs.getAllFilms().size() == size);
-        }
-        catch (HibernateException e) {
-            assert(false);
-        }
+
+        Assert.assertEquals(rs.getAllFilms().size(), size + 1);
+        Film tmp = rs.loadFilm(f.getId());
+        Assert.assertNotNull(tmp);
+        Assert.assertTrue(equals(tmp.getId(), f.getId()));
+        Assert.assertTrue(equals(tmp.getName(), f.getName()));
+        Assert.assertTrue(equals(tmp.getCompany(), f.getCompany()));
+        Assert.assertTrue(equals(tmp.getProdusr(), f.getProdusr()));
+        Assert.assertTrue(equals(tmp.getCdCost(), f.getCdCost()));
+        Assert.assertTrue(equals(tmp.getCasetteCost(), f.getCasetteCost()));
+
+        f.setName("UPDATED");
+        rs.updateFilm(f);
+        tmp = rs.loadFilm(f.getId());
+        Assert.assertTrue(equals(f.getName(), tmp.getName()));
+        rs.deleteFilm(tmp);
+        Assert.assertEquals(rs.getAllFilms().size(), size.intValue());
+
 
 
     }
@@ -86,31 +81,29 @@ public class DAOTest {
         RentServiceDAOImpl rs = new RentServiceDAOImpl();
         Integer size = null;
         List<Copy> list = null;
-        try {
-            list = rs.getAllCopys();
-        } catch (HibernateException e) {
-            assert (false);
-        }
+
+        list = rs.getAllCopys();
+
         size = list.size();
-        assert (size != null);
+        Assert.assertNotNull (size);
 
         Copy c = new Copy();
         c.setFilm(rs.loadFilm(fId));
         c.setType(isCd);
-        try {
-            rs.addCopy(c);
-            assert (rs.getAllCopys().size() == size + 1);
-            Copy tmp = rs.loadCopy(c.getId());
-            assert (tmp != null);
-            assert (equals(tmp.getId(), c.getId()));
-            assert (equals(tmp.getFilm().getId(), c.getFilm().getId()));
-            rs.deleteCopy(tmp);
-            assert (rs.getAllCopys().size() == size);
-            list = rs.getCopyByFilm(fId);
-            assert (!list.contains(tmp));
-        } catch (HibernateException e) {
-            assert (false);
-        }
+
+        rs.addCopy(c);
+        Assert.assertEquals (rs.getAllCopys().size(), size + 1);
+        Copy tmp = rs.loadCopy(c.getId());
+        Assert.assertNotNull (tmp);
+        Assert.assertTrue (equals(tmp.getId(), c.getId()));
+        Assert.assertTrue (equals(tmp.getFilm().getId(), c.getFilm().getId()));
+
+
+        rs.deleteCopy(tmp);
+        Assert.assertEquals (rs.getAllCopys().size(), size.intValue());
+        list = rs.getCopyByFilm(fId);
+        Assert.assertFalse (list.contains(tmp));
+
     }
     @DataProvider
     public Object[][] copyTestData() {
@@ -130,13 +123,11 @@ public class DAOTest {
         RentServiceDAOImpl rs = new RentServiceDAOImpl();
         Integer size = null;
         List<Customer> list = null;
-        try {
-            list = rs.getAllCustomers();
-        } catch (HibernateException e) {
-            assert (false);
-        }
+
+        list = rs.getAllCustomers();
+
         size = list.size();
-        assert (size != null);
+        Assert.assertNotNull (size);
 
         Customer c = new Customer();
         c.setFirstName(firstName);
@@ -144,27 +135,40 @@ public class DAOTest {
         c.setSurname(surname);
         c.setPatronymic(surname);
         c.setPhone(phone);
-        try {
-            rs.addCustomer(c);
-            assert (rs.getAllCustomers().size() == size + 1);
-            Customer tmp = rs.loadCustomer(c.getId());
-            assert (tmp != null);
-            assert (equals(tmp.getId(), c.getId()));
-            assert (equals(tmp.getFirstName(), c.getFirstName()));
-            assert (equals(tmp.getSurname(), c.getSurname()));
-            assert (equals(tmp.getAddress(), c.getAddress()));
-            assert (equals(tmp.getPhone(), c.getPhone()));
-            rs.deleteCustomer(tmp);
-            assert (rs.getAllCustomers().size() == size);
-            assert (equals(rs.getUnpayedCustomerByFilm(0).size(), 0));
-            assert (equals(rs.getUnpayedCustomerByFilm(1).size(), 1));
-            assert (equals(rs.getCustomerByCopy(1).size(), 1));
-            assert (equals(rs.getCustomerByCopy(3).size(), 2));
-            assert (equals(rs.getCustomerByRent(1).get(0).getId(), 1));
 
-        } catch (HibernateException e) {
-            assert (false);
+        rs.addCustomer(c);
+        Assert.assertEquals (rs.getAllCustomers().size(), size + 1);
+        Customer tmp = rs.loadCustomer(c.getId());
+        Assert.assertNotNull (tmp);
+        Assert.assertTrue (equals(tmp.getId(), c.getId()));
+        Assert.assertTrue (equals(tmp.getFirstName(), c.getFirstName()));
+        Assert.assertTrue (equals(tmp.getSurname(), c.getSurname()));
+        Assert.assertTrue (equals(tmp.getAddress(), c.getAddress()));
+        Assert.assertTrue (equals(tmp.getPhone(), c.getPhone()));
+
+        boolean exec = false;
+        try {
+            c.setPhone("88888888888");
+            rs.updateCustomer(c);
         }
+        catch (Exception e) {
+            exec = true;
+        }
+        Assert.assertTrue(exec);
+
+        c.setPhone("8888888888");
+        rs.updateCustomer(c);
+        tmp = rs.loadCustomer(c.getId());
+        Assert.assertTrue(equals(c.getPhone(), tmp.getPhone()));
+
+        rs.deleteCustomer(tmp);
+        Assert.assertEquals (rs.getAllCustomers().size(), size.intValue());
+        Assert.assertTrue (equals(rs.getUnpayedCustomerByFilm(0).size(), 0));
+        Assert.assertTrue (equals(rs.getUnpayedCustomerByFilm(1).size(), 1));
+        Assert.assertTrue (equals(rs.getCustomerByCopy(1).size(), 1));
+        Assert.assertTrue (equals(rs.getCustomerByCopy(3).size(), 2));
+        Assert.assertTrue (equals(rs.getCustomerByRent(1).get(0).getId(), 1));
+
     }
     @DataProvider
     public Object[][] customerTestData() {
@@ -185,14 +189,11 @@ public class DAOTest {
         RentServiceDAOImpl rs = new RentServiceDAOImpl();
         Integer size = null;
         List<Rent> list = null;
-        try {
-            list = rs.getAllRents();
-        }
-        catch (HibernateException e) {
-            assert(false);
-        }
+
+        list = rs.getAllRents();
+
         size = list.size();
-        assert(size != null);
+        Assert.assertNotNull(size);
 
         Rent r = new Rent();
         r.setStartDate(startDate);
@@ -200,32 +201,31 @@ public class DAOTest {
         r.setCopy(rs.loadCopy(cId));
         r.setCustomer(rs.loadCustomer(cId));
         r.setPayed(payed);
-        try {
-            rs.addRent(r);
 
-        }
-        catch (HibernateException e) {
-            assert(false);
-        }
+        rs.addRent(r);
 
-        try {
-            assert(rs.getAllRents().size() == size + 1);
-            Rent tmp = rs.loadRent(r.getId());
-            assert(tmp != null);
-            assert(equals(tmp.getId(), r.getId()));
-            assert(equals(tmp.getCustomer().getId(), r.getCustomer().getId()));
-            assert(equals(tmp.getCopy().getId(), r.getCopy().getId()));
-            assert(equals(tmp.getStartDate(), r.getStartDate()));
-            assert(equals(tmp.getEndDate(), r.getEndDate()));
-            assert(equals(tmp.getPayed(), r.getPayed()));
-            rs.deleteRent(tmp);
-            assert(rs.getAllRents().size() == size);
-            assert (equals(rs.getRentByCustomer(1).size(), 2));
-            assert (equals(rs.getRentByCustomer(1).get(0).getId(), 1));
-        }
-        catch (HibernateException e) {
-            assert(false);
-        }
+
+
+        Assert.assertEquals(rs.getAllRents().size(), size + 1);
+        Rent tmp = rs.loadRent(r.getId());
+        Assert.assertNotNull(tmp);
+        Assert.assertTrue(equals(tmp.getId(), r.getId()));
+        Assert.assertTrue(equals(tmp.getCustomer().getId(), r.getCustomer().getId()));
+        Assert.assertTrue(equals(tmp.getCopy().getId(), r.getCopy().getId()));
+        Assert.assertTrue(equals(tmp.getStartDate(), r.getStartDate()));
+        Assert.assertTrue(equals(tmp.getEndDate(), r.getEndDate()));
+        Assert.assertTrue(equals(tmp.getPayed(), r.getPayed()));
+
+        r.setPayed(0);
+        rs.updateRent(r);
+        tmp = rs.loadRent(r.getId());
+        Assert.assertTrue(equals(r.getPayed(), r.getPayed()));
+
+        rs.deleteRent(tmp);
+        Assert.assertEquals(rs.getAllRents().size(), size.intValue());
+        Assert.assertTrue (equals(rs.getRentByCustomer(1).size(), 2));
+        Assert.assertTrue (equals(rs.getRentByCustomer(1).get(0).getId(), 1));
+
 
 
     }
